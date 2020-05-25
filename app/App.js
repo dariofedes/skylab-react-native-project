1daf760b00657b6585dc06dd0392d38bd5b6851e
Darío Fernández <dariofedes@protonmail.com>
2020-05-25 09:47:16 +0200

    initializing project

---
+++ app/App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native'
+
+import styles from './styles'
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+export default function App(props) {
+  return <SafeAreaView style={styles.container}>
+    <Text style={styles.title}>Hello World!</Text>
+  </SafeAreaView>
+}
