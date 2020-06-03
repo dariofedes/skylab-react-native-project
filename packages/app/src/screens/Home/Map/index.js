092db3d3c576ce0f43aa6420cd2666ac2bcae56f
Maider Hernandorena <maiderhernandorena@Maiders-MacBook-Pro.local>
2020-06-03 22:39:03 +0200

    fix ios permissions and add services

--- packages/app/src/screens/Home/Map/index.js
+++ packages/app/src/screens/Home/Map/index.js
@@ -1,57 +1,36 @@
 import React, { useEffect, useState, useRef } from 'react'
-import { View, Platform } from 'react-native'
-import Geolocation from '@react-native-community/geolocation';
-import { check, PERMISSIONS } from 'react-native-permissions';
 import MapView from 'react-native-maps';
 
-import Text from '../../../components/commons/text'
-import styles from './styles';
+import { requestLocationPermissions } from '@skylab/services/src/Permissions'
+import { requestLocation } from '@skylab/services/src/Geolocation'
 
-const LOCATION = PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION;
+import styles from './styles';
 
 const LATITUDE_DELTA = 0.0922
 const LONGITUDE_DELTA = 0.0421
 
 export default function Map() {
     let _map = useRef(null)
-
     const [coords, setCoords] = useState(null)
 
     useEffect(() => {
-        requestPermissions()
+        getLocation()
     }, [])
 
-    const requestPermissions = async() => {
-        if (Platform.OS === 'android') {
-            await check(LOCATION)
+    const getLocation = async() => {
+        const res = await requestLocationPermissions()
+
+        if (res) {
+            requestLocation(({ coords }) => {
+                setCoords(coords);
+    
+                _map.current.animateToCoordinate({
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                }, 1)
+            })
+
         }
-
-        requestLocation()
-    }
-
-    const requestLocation = async() => {
-        const options = {
-            timeout: 3000,
-            maximumAge: 2000,
-            enableHighAccuracy: true
-        }
-
-        Geolocation.getCurrentPosition(getPosition, getError, options)
-    }
-
-    const getPosition = ({ coords }) => {
-        if (coords) {
-            setCoords(coords);
-
-            _map.current.animateToCoordinate({
-                latitude: coords.latitude,
-                longitude: coords.longitude
-            }, 1)
-        }
-    }
-
-    const getError = (error) => {
-        console.log(error)
     }
 
     return (
